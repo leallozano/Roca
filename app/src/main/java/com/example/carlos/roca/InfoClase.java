1c1f04e3d4cffe6f3176233cd5d51fd3a69a19c9
Carlos <leal.lozano@gmail.com>
2017-05-24 22:50:51 -0400

    Recursos 1 Activity

---
+++ app/src/main/java/com/example/carlos/roca/InfoClase.java
@@ -0,0 +1,18 @@
+package com.example.carlos.roca;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Carlos on 5/24/2017.
+ */
+
+public class InfoClase extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+
+
+    }
+}
--- app/src/main/java/com/example/carlos/roca/MainActivity.java
+++ app/src/main/java/com/example/carlos/roca/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.carlos.roca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void salirApp (View view) {
+        finish();
+    }
+
+    public void ejecutar_info(View view) {
+        Intent i = new Intent(this,InfoClase.class);
+        startActivity(i);
     }
 }
